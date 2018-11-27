671f9523cb7413d8fddb8bf7c520ff6784592509
Marcos Villegas <dinavers@gmail.com>
2018-11-26 21:34:46 -0300

    ejercicio3

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,26 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+
+#i=1
+#990.times do
+#  if 990%i==0
+#    puts i
+#  end
+#  i= i+1
+#end
+
+
+#i=1
+#while i < 991
+#  if 990%i==0
+#    puts i
+#  end
+#  i=i+1
+#end
+
+
+for i in 1..990
+  if 990%i==0
+    puts i
+  end
+end
