ea5151a4486c021ede7d6c4ccd92125d5e86fbc5
Marcos Villegas <dinavers@gmail.com>
2018-11-27 22:14:43 -0300

    ejercicio9

--- 2 ciclos/ejercicio9.rb
+++ 2 ciclos/ejercicio9.rb
@@ -11,3 +11,14 @@
 </table>
 
 =end
+
+puts '<table>'
+puts ' <tbody>'
+puts '  <tr>'
+3.times do |i|
+  i += 1
+  puts "   <td> #{i} </td>\n"
+end
+puts '  </tr>'
+puts ' </tbody>'
+puts '</table>'
