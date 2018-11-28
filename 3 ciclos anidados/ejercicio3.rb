7a04874270d7ce23493c307e4bf6185ccf3312df
Marcos Villegas <dinavers@gmail.com>
2018-11-28 00:37:08 -0300

    ejercicio3

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,16 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+
+
+
+puts 'ingrese un numero (0 para salir)'
+n = gets.to_i
+while n != 0
+  for i in 1..10
+    mul = i * n
+    puts "#{n} * #{i} = #{mul}"
+  end
+  puts 'ingrese un numero (0 para salir)'
+  n = gets.to_i
+end
